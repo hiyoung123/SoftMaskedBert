2e62c7e977c22146e0590df91821b173ca8f3ae4
hiyoung <hiyoungliu@gmail.com>
2020-07-08 10:58:40 +0800

    add

--- train.py
+++ train.py
@@ -20,16 +20,15 @@ class SoftMaskedBertTrainer():
     def __init__(self, bert, tokenizer, device, hidden=256, layer_n=1, lr=2e-5, gama=0.8, betas=(0.9, 0.999), weight_decay=0.01, warmup_steps=10000):
 
         self.device = device
-        self.bert = bert
         self.tokenizer = tokenizer
-        self.model = SoftMaskedBert(self.bert, self.tokenizer, hidden, layer_n, self.device).to(self.device)
+        self.model = SoftMaskedBert(bert, self.tokenizer, hidden, layer_n, self.device).to(self.device)
 
         # if torch.cuda.device_count() > 1:
         #     print("Using %d GPUS for train" % torch.cuda.device_count())
         #     self.model = nn.DataParallel(self.model, device_ids=[0,1,2])
 
-        self.optim = Adam(self.model.parameters(), lr=lr, betas=betas, weight_decay=weight_decay)
-        self.optim_schedule = ScheduledOptim(self.optim, hidden, n_warmup_steps=warmup_steps)
+        optim = Adam(self.model.parameters(), lr=lr, betas=betas, weight_decay=weight_decay)
+        self.optim_schedule = ScheduledOptim(optim, hidden, n_warmup_steps=warmup_steps)
         self.criterion_c = nn.NLLLoss()
         self.criterion_d = nn.BCELoss()
         self.gama = gama
@@ -46,11 +45,11 @@ class SoftMaskedBertTrainer():
     def inference(self, data_loader):
         self.model.eval()
         out_put = []
-        data_iter = tqdm.tqdm(enumerate(data_loader),
+        data_loader = tqdm.tqdm(enumerate(data_loader),
                               desc="%s" % 'Inference:',
                               total=len(data_loader),
                               bar_format="{l_bar}{r_bar}")
-        for i, data in data_iter:
+        for i, data in data_loader:
             # 0. batch_data will be sent into the device(GPU or cpu)
             data = {key: value.to(self.device) for key, value in data.items()}
 
@@ -193,9 +192,9 @@ if __name__ == '__main__':
         train = dataset.iloc[train_index]
         val = dataset.iloc[val_index]
         train = BertDataset(tokenizer, train, max_len=152)
-        train = DataLoader(train, batch_size=8, num_workers=2)
+        train = DataLoader(train, batch_size=16, num_workers=2)
         val = BertDataset(tokenizer, val, max_len=152)
-        val = DataLoader(val, batch_size=8, num_workers=2)
+        val = DataLoader(val, batch_size=16, num_workers=2)
         trainer = SoftMaskedBertTrainer(bert, tokenizer, device)
         best_loss = 100000
         for e in range(100):
